17ee068607541d1127dea510929b770d62656ed5
lukhed <lukhed.mail@gmail.com>
2024-12-23 20:32:57 -0500

    restructure repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nfl_stadiums",
-    version="0.9.0",
+    version="1.0.0",
     description="A simple python package that provides easy access to NFL stadium data",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
